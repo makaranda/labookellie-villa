81b3cd44c184f892415e595929e04538b2167129
Makaranda Pathirana <32447124+makaranda@users.noreply.github.com>
2024-02-01 15:31:19 +0530

    edited 001

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,32 @@ Route::get('/', function () {
     return view('pages.home.index');
 });
 
+Route::get('/about-us', function () {
+    return view('pages.about.index');
+});
+
+Route::get('/gallery', function () {
+    return view('pages.gallery.index');
+});
+
+Route::get('/contact-us', function () {
+    return view('pages.contact.index');
+});
+
+Route::get('/rooms/room-name1', function() {
+    return view('pages.rooms.room_name1');
+});
+
+Route::get('/rooms/room-name2', function() {
+    return view('pages.rooms.room_name2');
+});
+
+Route::get('/rooms/room-name3', function() {
+    return view('pages.rooms.room_name3');
+});
 
 Route::get('/accommodation/rooms', [HotelRoomsController::class, 'showData']);
-Route::get('/accommodation/room/{room_url}', [HotelRoomsController::class, 'singleRoom']);
+//Route::get('/accommodation/room/{room_url}', [HotelRoomsController::class, 'singleRoom']);
 Route::get('/dining/restaurant/tides-restaurant', function () {
     return view('pages.tides-restaurant.index');
 });
