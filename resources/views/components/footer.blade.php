88729b18d722e60f978c32215f6f875d7eba2d45
Makaranda Pathirana <32447124+makaranda@users.noreply.github.com>
2024-02-02 11:29:18 +0530

    edited 004564

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,21 +6,18 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-                        <h4>Top Products</h4>
+                        <h4>Our Partners</h4>
                         <ul>
-                            <li><a href="#">Managed Website</a></li>
-                            <li><a href="#">Manage Reputation</a></li>
-                            <li><a href="#">Power Tools</a></li>
-                            <li><a href="#">Marketing Service</a></li>
+                            <li><a href="http://marinobeach.com" target="_new">Marino Beach</a></li>
+                            <li><a href="https://sigiriyaforestedge.com" target="_new">Sigiriya Forestedge</a></li>
                         </ul>
                     </div>
                     <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
                         <h4>Quick Links</h4>
                         <ul>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Brand Assets</a></li>
-                            <li><a href="#">Investor Relations</a></li>
-                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="{{ URL::to('') }}/about-us">About us</a></li>
+                            <li><a href="{{ URL::to('') }}/gallery">Gallery</a></li>
+                            <li><a href="{{ URL::to('') }}/contact-us">Contact us</a></li>
                         </ul>
                     </div>
                     <div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,6 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Labookele Vila | Bungalow</title>
+        <link rel="icon" href="{{ URL::to('') }}/resources/images/favicon.png" type="image/png">
         <meta name="keywords" content="Hotels,  Bungalow in Sri Lanka">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
